56b1af7299cfd93bca2373af158f9dcfaf45b91e
chenxiang <704991100@qq.com>
2025-07-31 16:35:00 +0800

    优化RNBarLineChartBaseManager中的视图获取逻辑，使用uiManager的view(forReactTag:)方法替代直接从viewRegistry中获取视图，提升代码的可读性和安全性。

--- ios/ReactNativeCharts/RNBarLineChartBaseManager.swift
+++ ios/ReactNativeCharts/RNBarLineChartBaseManager.swift
@@ -13,35 +13,35 @@ protocol RNBarLineChartBaseManager {
 extension RNBarLineChartBaseManager {
   func _moveViewToX(_ reactTag: NSNumber, xValue: NSNumber) {
     _bridge?.uiManager.addUIBlock { (uiManager: RCTUIManager?, viewRegistry:[NSNumber : UIView]?) in
-      let view: RNBarLineChartViewBase = viewRegistry![reactTag] as! RNBarLineChartViewBase;
+      let view: RNBarLineChartViewBase = uiManager?.view(forReactTag: reactTag) as! RNBarLineChartViewBase;
       (view.chart as! BarLineChartViewBase).moveViewToX(xValue.doubleValue);
     }
   }
 
   func _moveViewTo(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString) {
     _bridge?.uiManager.addUIBlock { (uiManager: RCTUIManager?, viewRegistry:[NSNumber : UIView]?) in
-      let view: RNBarLineChartViewBase = viewRegistry![reactTag] as! RNBarLineChartViewBase;
+      let view: RNBarLineChartViewBase = uiManager?.view(forReactTag: reactTag) as! RNBarLineChartViewBase;
       (view.chart as! BarLineChartViewBase).moveViewTo(xValue: xValue.doubleValue, yValue: yValue.doubleValue, axis: BridgeUtils.parseAxisDependency(axisDependency as String));
     }
   }
 
   func _moveViewToAnimated(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString, duration: NSNumber) {
     _bridge?.uiManager.addUIBlock { (uiManager: RCTUIManager?, viewRegistry:[NSNumber : UIView]?) in
-      let view: RNBarLineChartViewBase = viewRegistry![reactTag] as! RNBarLineChartViewBase;
+      let view: RNBarLineChartViewBase = uiManager?.view(forReactTag: reactTag) as! RNBarLineChartViewBase;
       (view.chart as! BarLineChartViewBase).moveViewToAnimated(xValue: xValue.doubleValue, yValue: yValue.doubleValue, axis: BridgeUtils.parseAxisDependency(axisDependency as String), duration: duration.doubleValue / 1000.0);
     }
   }
 
   func _centerViewTo(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString) {
     _bridge?.uiManager.addUIBlock { (uiManager: RCTUIManager?, viewRegistry:[NSNumber : UIView]?) in
-      let view: RNBarLineChartViewBase = viewRegistry![reactTag] as! RNBarLineChartViewBase;
+      let view: RNBarLineChartViewBase = uiManager?.view(forReactTag: reactTag) as! RNBarLineChartViewBase;
       (view.chart as! BarLineChartViewBase).centerViewTo(xValue: xValue.doubleValue, yValue: yValue.doubleValue, axis: BridgeUtils.parseAxisDependency(axisDependency as String));
     }
   }
 
   func _centerViewToAnimated(_ reactTag: NSNumber, xValue: NSNumber, yValue: NSNumber, axisDependency: NSString, duration: NSNumber) {
     _bridge?.uiManager.addUIBlock { (uiManager: RCTUIManager?, viewRegistry:[NSNumber : UIView]?) in
-      let view: RNBarLineChartViewBase = viewRegistry![reactTag] as! RNBarLineChartViewBase;
+      let view: RNBarLineChartViewBase = uiManager?.view(forReactTag: reactTag) as! RNBarLineChartViewBase;
       (view.chart as! BarLineChartViewBase).centerViewToAnimated(xValue: xValue.doubleValue, yValue: yValue.doubleValue, axis: BridgeUtils.parseAxisDependency(axisDependency as String), duration: duration.doubleValue / 1000.0);
     }
   }
@@ -55,14 +55,14 @@ extension RNBarLineChartBaseManager {
 
   func _highlights(_ reactTag: NSNumber, config: NSArray) {
     _bridge?.uiManager.addUIBlock { (uiManager: RCTUIManager?, viewRegistry:[NSNumber : UIView]?) in
-      let view: RNBarLineChartViewBase = viewRegistry![reactTag] as! RNBarLineChartViewBase;
+      let view: RNBarLineChartViewBase = uiManager?.view(forReactTag: reactTag) as! RNBarLineChartViewBase;
       (view.chart as! BarLineChartViewBase).highlightValues(HighlightUtils.getHighlights(config));
     }
   }
 
   func _setDataAndLockIndex(_ reactTag: NSNumber, data: NSDictionary) {
     _bridge?.uiManager.addUIBlock { (uiManager: RCTUIManager?, viewRegistry:[NSNumber : UIView]?) in
-      let view: RNBarLineChartViewBase = viewRegistry![reactTag] as! RNBarLineChartViewBase;
+      let view: RNBarLineChartViewBase = uiManager?.view(forReactTag: reactTag) as! RNBarLineChartViewBase;
       view.setDataAndLockIndex(data);
     }
   }
